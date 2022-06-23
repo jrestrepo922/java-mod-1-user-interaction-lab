3270f7be8b7407c0834e1699f6fbe63133ba6ad7
Juan Martin Restrepo <tinto922@gmail.com>
2022-06-23 16:30:30 -0400

    User Interaction Lab complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // your code here
+        System.out.println("Please enter you age");
+        try( Scanner scanner = new Scanner(System.in)){
+            int input = scanner.nextInt();
+            if(input < 18){
+                System.out.println("Hello teenager");
+            } else if(input == 18){
+                System.out.println("Hello new adult");
+            } else if(input > 18){
+                System.out.println("Hello aged fellow");
+            }
+        } catch(Exception e){
+            System.out.println("Something when wrong");
+        }
+       
     }
 }
